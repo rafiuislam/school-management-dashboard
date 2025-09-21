677bb1ca4108a0b4aa3ed8e55983a3e236fdd419
Rafiu Islam <rafiuislam123@gmail.com>
2025-09-21 14:52:37 +0600

    Initial commit

---
+++ src/app/(dashboard)/layout.jsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import Link from "next/link";
+import Menu from "@/app/components/Menu";
+
+export default function DashboardLayout({ children }) {
+  return (
+    <div className="min-h-screen flex">
+      {/* Left */}
+      <div className="w-[15%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4 bg-red-300">
+        <Link
+          href="/"
+          className="flex items-center justify-center lg:justify-start gap-2"
+        >
+          <Image src="/logo.png" alt="logo-png" width={32} height={32} />
+          <span className="hidden lg:block">School Management</span>
+        </Link>
+        <Menu />
+      </div>
+      <div className="w-[85%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-blue-200">
+        R
+      </div>
+    </div>
+  );
+}
