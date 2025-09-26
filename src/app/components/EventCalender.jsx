4d5d2115ebbb8eef69a7039832dd32bf2a81b3a6
Rafiu Islam <rafiuislam123@gmail.com>
2025-09-26 17:05:07 +0600

    EventCalender Ui added

--- src/app/components/EventCalender.jsx
+++ src/app/components/EventCalender.jsx
@@ -1,4 +1,5 @@
 "use client";
+import Image from "next/image";
 import { useState } from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
@@ -34,8 +35,26 @@ const events = [
 const EventCalender = () => {
   const [value, onChange] = useState(new Date());
   return (
-    <div className="bg-white p-4 rounded-lg ">
+    <div className="bg-white p-4 rounded-md">
       <Calendar onChange={onChange} value={value} />
+      <div className="flex items-center justify-between">
+        <h1 className="text-xl font-semibold my-4">Events</h1>
+        <Image src="/moreDark.png" alt="" width={20} height={20} />
+      </div>
+      <div className="flex flex-col gap-4">
+        {events.map((event) => (
+          <div
+            className="p-5 rounded-md border-2 border-gray-100 border-t-4 odd:border-t-cSky even:border-t-cPurple"
+            key={event.id}
+          >
+            <div className="flex items-center justify-between">
+              <h1 className="font-semibold text-gray-600">{event.title}</h1>
+              <span className="text-gray-300 text-xs">{event.time}</span>
+            </div>
+            <p className="mt-2 text-gray-400 text-sm">{event.description}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
