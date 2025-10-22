fa66b294bbc92a9a0a441c242dd3f4e583d3d3d4
Rafiu Islam <rafiuislam123@gmail.com>
2025-10-22 13:16:02 +0600

    techer, parent and student page done

--- src/app/(dashboard)/layout.jsx
+++ src/app/(dashboard)/layout.jsx
@@ -17,7 +17,7 @@ export default function DashboardLayout({ children }) {
         </Link>
         <Menu />
       </div>
-      <div className="w-[85%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll">
+      <div className="w-[85%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll flex flex-col">
         <Navbar />
         {children}
       </div>
--- src/app/(dashboard)/teacher/page.jsx
+++ src/app/(dashboard)/teacher/page.jsx
@@ -1,7 +1,24 @@
+import BigCalendar from "../../components/BigCalender";
+import Announcements from "../../components/Announcements";
+// import EventCalender from "../../components/EventCalender";
+
 const TeacherPage = () => {
   return (
-    <div>TeacherPage</div>
-  )
-}
+    <div className="flex-1 flex flex-col xl:flex-row p-4 gap-4">
+      {/* left */}
+      <div className="w-full xl:w-2/3 flex flex-col gap-8 bg-amber-400">
+        <div className="h-full bg-white p-4 rounded-md">
+          <h1 className="text-xl font-semibold">Schedule </h1>
+          <BigCalendar />
+        </div>
+      </div>
+      {/* right */}
+      <div className="w-full xl:w-1/3 flex flex-col gap-8 ">
+        {/* <EventCalender /> */}
+        <Announcements />
+      </div>
+    </div>
+  );
+};
 
-export default TeacherPage
+export default TeacherPage;
